6a8f86e89f46b7896386c05e2a8b2d4251d401a7
Rafi Haykal <devsoftware80@gmail.com>
2026-03-05 15:49:02 +0700

    Menambahkan kondisi session untuk direktur

--- index.php
+++ index.php
@@ -8,6 +8,9 @@ if (isset($_SESSION['role'])) {
     } elseif ($_SESSION['role'] == "lo") {
         header("Location:" . BASE_URL . "lo/dashboard.php");
         exit;
+    }elseif ($_SESSION['role'] == "direktur") {
+        header("Location:" . BASE_URL . "direktur/dashboard.php");
+        exit;
     }
 }
 ?>
@@ -61,32 +64,6 @@ if (isset($_SESSION['role'])) {
             </svg>
             <div>
                 Email atau password tidak valid. Silakan coba lagi.
-            </div>
-        </div>
-        <script>
-            setTimeout(() => {
-                const alertEl = document.getElementById('logoutAlert');
-                if (alertEl) {
-                    const bsAlert = new bootstrap.Alert(alertEl);
-                    bsAlert.close();
-                }
-            }, <?= $durasi ?>);
-            window.history.replaceState({}, document.title, 'index.php');
-        </script>
-    <?php
-    }
-    ?>
-
-    <?php
-    if (isset($_GET['status']) && $_GET['status'] == 'off') {
-        $durasi = 4000; // durasi dalam ms (4 detik)
-    ?>
-        <div id="logoutAlert" class="alert alert-warning d-flex position-absolute w-100 align-items-center fade show" role="alert">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
-                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-            </svg>
-            <div>
-                Akun Anda sedang dinonaktifkan. Hubungi administrator untuk bantuan.
             </div>
         </div>
         <script>
