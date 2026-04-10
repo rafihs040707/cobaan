9ba3cb2770e74dd0eae629f92b519981b63a0da9
Rafi Haykal <devsoftware80@gmail.com>
2026-04-10 15:59:17 +0700

    Perubahan di query, sekarang sudah pakai prepared statement

--- admin/user/proses_reset.php
+++ admin/user/proses_reset.php
@@ -5,18 +5,18 @@ require_once BASE_PATH . "/auth/cek_login.php";
 require_once BASE_PATH . "/config/config.php";
 
 $id = $_POST['id'];
-
 $password = $_POST['password'];
 
-/* hash password */
+if (!ctype_digit($id)) die("ID tidak valid");
 
+/* hash password */
 $password_hash = password_hash($password, PASSWORD_DEFAULT);
 
-/* update password */
-
-mysqli_query($conn,"UPDATE users SET password='$password_hash' WHERE id='$id'");
+/* update password (AMAN) */
+$stmt = mysqli_prepare($conn, "UPDATE users SET password=? WHERE id=?");
+mysqli_stmt_bind_param($stmt, "si", $password_hash, $id);
+mysqli_stmt_execute($stmt);
 
 /* redirect */
-
 header("Location:" . BASE_URL . "admin/user/index.php");
 exit;
